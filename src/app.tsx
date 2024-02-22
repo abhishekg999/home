34b8677a106502cd30696efe67fbca6e02b54dc1
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2024-02-21 18:15:24 -0800

    base

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "./components/Navbar";
+export function App() {
+    return (
+        <>
+            <Navbar />
+            <div className="flex justify-center items-center text-center flex-1 text-6xl">
+                Work in Progress
+            </div>
+        </>
+    );
+}
