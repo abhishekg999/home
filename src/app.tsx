6dd57e9079b50ac9ca9a05f4e374cb1c44d54acd
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2024-02-24 17:10:26 -0800

    page structure kinda like this? plus scrollbar

--- src/app.tsx
+++ src/app.tsx
@@ -4,8 +4,14 @@ export function App() {
     return (
         <>
             <Navbar />
-            <div className="flex justify-center items-center text-center flex-1 text-6xl">
-                Work in Progress
+            <div className="text-center">
+                <div className="h-[calc(100vh-4rem)] flex justify-center items-center text-3xl">
+                    Work in Progress
+                </div>
+                <div className="h-[calc(100vh-4rem)] flex justify-center items-center text-3xl">
+                    meow
+                </div>
+
             </div>
             {/*<!-- <Footer />*/}
         </>
