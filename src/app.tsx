8882cfba6068e161febede135e88244eab7f21b6
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2024-02-25 20:46:57 -0800

    animation + some page layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,32 +1,28 @@
 import { Navbar } from "./components/Navbar";
-// import { Footer } from "./components/Footer";
+import { Footer } from "./components/Footer";
 export function App() {
     return (
         <>
+            <div className="h-[100vh] relative">
             <Navbar />
-            <div className="">
-                <section class="h-[calc(100vh-4rem)] flex flex-col text-center m-8 py-12">
+            <div className="p-6">
+                <section class="flex flex-col text-center m-8 py-6">
                     <h1 class="text-5xl inset-2 font-medium mb-6 z-10">Abhishek Govindarasu</h1>
-        
-                    <div className="rounded-lg bg-[#1f2b28] p-4 mx-8 relative border-solid border-b-2 border-r-2 border-l-2 border-indigo-400">
-                        <p className="text-lg font-light text-[#ebf2fa]">
+
+                    <div className="rounded-lg bg-[#1f2b28] p-4 sm:mx-2 relative border-solid border-2 border-gray-500">
+                        <p className="text-md font-light text-[#ebf2fa]">
                             I am a developer and Computer Science student currently at <span className="text-green-600">UC San Diego</span>.
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam harum deleniti nobis omnis eius voluptatem molestias minus unde excepturi enim, nihil molestiae facilis dolorum reiciendis ad porro similique mollitia cum?
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse excepturi consequatur reprehenderit, maiores eaque expedita doloremque aliquam sint modi error impedit repellat? Molestias tempora molestiae natus quaerat. Quasi, debitis aliquid.
                         </p>
-
-                        {/* <div className="absolute bg-[#62929e] opacity-60 h-16 w-16 bottom-0 right-0 m-4"></div>
-                        <div className="absolute bg-[#62929e] opacity-60 h-16 w-16 bottom-0 right-0 m-4"></div>
-                        <div className="absolute bg-[#62929e] opacity-60 h-16 w-16 bottom-0 right-0 m-4"></div> */}
                     </div>
-
-    
                 </section>
-                <section className="h-[calc(100vh-4rem)] flex justify-center items-center">
-                    <p>meow</p>
+                <section className="flex">
+                    {/* <p className="text-lg">This page is work in progess.</p> */}
                 </section>
 
             </div>
-            {/*<!-- <Footer />*/}
+            <Footer />
+            </div>
         </>
     );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export function Footer() {
+    return (
+        <footer className="text-xs p-0 text-center ">
+            <p>© 2024 Abhishek Govindarasu | Work in Progress</p>
+        </footer>
+
+    );
+}
