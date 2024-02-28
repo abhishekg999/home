afd4bef920a65232b34241c0200ce80fd20d789d
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2024-02-28 01:31:07 -0800

    frontpage with contact me, github pages router 404, bulba animation

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,23 @@
+import CloseIcon from '@mui/icons-material/Close';
+import { useEffect, useState } from "preact/hooks";
+
 export function Home(_: any) {
+    const [open, setOpen] = useState(false);
+    const copyEmailToClipboard = () => {
+        navigator.clipboard.writeText("abhishekgovindarasu@gmail.com");
+        setOpen(true);
+    };
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setOpen(false);
+        }, 1500);
+        return () => clearTimeout(timer);
+    })
+
     return (
         <main className="flex flex-col justify-center items-center">
-            <section className="w-full pt-10 pb-8 flex justify-center items-center container max-w-[600px] px-4">
+            <section className="w-full pt-10 pb-8 flex flex-col justify-center items-center container max-w-[600px] px-4">
                 <div className="container flex flex-col items-center justify-center space-y-4">
                     <div className="text-center">
                         <h1 className="text-5xl font-bold tracking-tighter pt-6 text-white">
@@ -9,13 +25,42 @@ export function Home(_: any) {
                         </h1>
 
                         <div className="text-gray-300 px-6 pt-2">
-                            I'm a Computer Science student at UC San Diego.
+                            I'm a Computer Science student at UC San Diego. In
+                            my free time, I am a Capture the Flag (CTF) player
+                            specializing in web exploitation and reverse
+                            engineering.
                         </div>
                     </div>
                     <div
                         className="container space-y-4 flex flex-col items-center bg-[url(https://images.unsplash.com/photo-1706922926159-d32db3419657?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTcwOTAxNzc4MQ&ixlib=rb-4.0.3&q=80&w=1080)] bg-[100%_auto] max-w-[60rem] max-h-[30rem] aspect-video rounded-2xl"
                         alt="placeholder"
                     ></div>
+                </div>
+
+                <hr className="w-5/6 max-w-[480px] my-12"></hr>
+
+
+                {/* Last thing of main page will be this */}
+                <div className="mt-6">
+                    <a
+                        className="p-2 px-6 border-[1px] border-white rounded-md hover:bg-white hover:text-black transition-colors duration-300 ease-in-out"
+                        href="mailto:abhishekgovindarasu@gmail.com"
+                        onClick={copyEmailToClipboard}
+                    >
+                        Contact Me
+                    </a>
+
+
+                    {open ? (
+                        <div className="text-sm flex justify-between fixed bottom-0 bg-gray left-0 m-4 px-4 py-2 min-w-[280px] max-w-[320px] border-2 rounded-lg clipboard-popup">
+                            <p> Copied to Clipboard </p>
+                            <CloseIcon
+                                onClick={() => setOpen(false)}
+                                className="cursor-pointer hover:"
+                            >
+                            </CloseIcon>
+                        </div>
+                    ) : null}
                 </div>
             </section>
         </main>
