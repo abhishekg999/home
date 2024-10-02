cea4b4e947a95c3373426f4cbffcbafcdabb7b6b
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2024-10-02 00:07:44 -0700

    better head tags prerender

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -4,7 +4,7 @@ export function Error({ path }: RoutableProps) {
   routerPage.value = "Error";
 
   return (
-    <section class="error w-full pt-10 pb-8 flex flex-col justify-center items-center container max-w-[600px] p-4 my-4 m-[0_auto]">
+    <section class="error w-full pt-16 pb-8 flex flex-col justify-center items-center container max-w-[600px] p-4 my-4 m-[0_auto] z-20">
       <p>
         Oops that page does not exist. If you are seeing this now, its probably
         cause this site is still in development, check back later!
--- src/main.tsx
+++ src/main.tsx
@@ -8,5 +8,49 @@ if (typeof window !== 'undefined') {
 }
 
 export async function prerender(data: any) {
-    return await ssr(<App {...data} />)
+    const { url } = data;
+    const { html, links } = await ssr(<App {...data} />);
+
+    let title, description;
+    if (url === '/') {
+        title = 'Abhishek Govindarasu - Home';
+        description = "Hi, I'm Abhishek Govindarasu. a developer, computer science student, and a CTF player.";
+    }
+    else if (url === '/projects') {
+        title = 'Abhishek Govindarasu - Projects';
+        description = "Here are some of the projects I have worked on, I have many more work in progress projects that I will be adding soon!";
+    } 
+    else {
+        title = 'Abhishek Govindarasu - 404';
+        description = "The page you are looking for does not exist.";
+        console.error(`404: ${url}`);
+    }
+
+    return {
+        html,
+        links,
+        head: {
+            lang: 'en',
+            title: title,
+            elements: new Set([
+                { type: 'meta', props: { charset: 'UTF-8' } },
+                { type: 'link', props: { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' } },
+                { type: 'link', props: { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' } },
+                { type: 'link', props: { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' } },
+                { type: 'link', props: { rel: 'manifest', href: '/site.webmanifest' } },
+                { type: 'meta', props: { name: 'viewport', content: 'width=device-width, initial-scale=1.0' } },
+                { type: 'meta', props: { name: 'title', content: title } },
+                { type: 'meta', props: { name: 'description', content: description } },
+                { type: 'meta', props: { property: 'og:type', content: 'website' } },
+                { type: 'meta', props: { property: 'og:url', content: 'https://ahh.bet/' } },
+                { type: 'meta', props: { property: 'og:title', content: title } },
+                { type: 'meta', props: { property: 'og:description', content: description } },
+                { type: 'meta', props: { property: 'twitter:card', content: 'summary_large_image' } },
+                { type: 'meta', props: { property: 'twitter:url', content: 'https://ahh.bet/' } },
+                { type: 'meta', props: { property: 'twitter:title', content: title } },
+                { type: 'meta', props: { property: 'twitter:description', content: description } },
+                { type: 'meta', props: { name: 'theme-color', content: '#131E16' } }
+            ])
+        }
+    };
 }
