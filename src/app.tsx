6b055d313d49b56be86f0450ed0c77a0ca9d33e0
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2024-10-03 21:07:02 -0700

    fixed background image

--- src/app.tsx
+++ src/app.tsx
@@ -10,15 +10,20 @@ export function App() {
     <>
       <div className="flex flex-col min-h-[100dvh] relative">
         <div className="absolute inset-0 z-0 pointer-events-none">
+
+          {/** Very light gradients overlaying the image */}
           <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#1a2e1a] z-10 opacity-0"></div>
           <div className="absolute inset-0 bg-gradient-to-t from-transparent to-[#1a2e1a] z-10 opacity-0"></div>
           <div className="absolute inset-0 bg-gradient-to-r from-transparent to-[#1a2e1a] z-10 opacity-20"></div>
           <div className="absolute inset-0 bg-gradient-to-l from-transparent to-[#1a2e1a] z-10 opacity-20"></div>
-          <img
+
+          {/* <img
             src="/min/background-min.jpg"
             alt="Abstract background"
-            className="w-full h-full object-cover object-right-top opacity-40"
-          />
+            className="w-full h-full object-cover object-right-top bg-contain bg-fixed opacity-40"
+          /> */}
+          {/** Background image, always full size */}
+          <div className="w-full h-full bg-[url(/min/background-min.jpg)] bg-cover bg-fixed opacity-40 bg-right-top"></div>
         </div>
         <LocationProvider>
           <Navbar />
