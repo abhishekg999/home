7cf141800d4dedeb241fbdef3b925cf9a363e2f5
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2024-10-20 00:33:40 -0700

    refactor + webp background

--- src/app.tsx
+++ src/app.tsx
@@ -11,13 +11,7 @@ export function App() {
     <>
       <div className="flex flex-col min-h-[100dvh] relative">
         <Background />
-        <div className="absolute inset-0 z-0 pointer-events-none">
-          {/** Very light gradients overlaying the image */}
-          <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#1a2e1a] z-10 opacity-0"></div>
-          <div className="absolute inset-0 bg-gradient-to-t from-transparent to-[#1a2e1a] z-10 opacity-0"></div>
-          <div className="absolute inset-0 bg-gradient-to-r from-transparent to-[#1a2e1a] z-10 opacity-20"></div>
-          <div className="absolute inset-0 bg-gradient-to-l from-transparent to-[#1a2e1a] z-10 opacity-20"></div>
-        </div>
+        
         <LocationProvider>
           <Navbar />
           <Router>
